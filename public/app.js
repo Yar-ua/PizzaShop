36c9d3e27f12835d7bd90b55c1f4e4b0b586b1fc
Yar-ua <Yar.ua.1985@gmail.com>
2016-08-02 22:06:46 +0300

    view products on index page

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 function something()
 	{
-		var x = window.localStorage.getItem('bbb');
+		var x = window.localStorage.getItem('bbb');		// x = hh{'bbb'}
 
-		x = x*1 + 1;
+		x = x*1 + 1;									// x = x + 1, х*1 - преобразование строкового типа в численный
 
 		window.localStorage.setItem('bbb', x);
 
