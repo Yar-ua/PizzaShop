4df05fecf53875a60cb460a622cc3433f2e94aa2
Yar-ua <Yar.ua.1985@gmail.com>
2016-08-04 23:31:24 +0300

    add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -16,6 +16,8 @@ function add_to_cart(id)
 		var x = window.localStorage.getItem(key);
 		x = x * 1 + 1;
 		window.localStorage.setItem(key, x);
+
+		alert('Items in your cart: ' + cart_get_number_of_items());		//вывод кол-ва Itemов в корзине
 	}
 
 
