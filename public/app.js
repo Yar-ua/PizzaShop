0aa095e83764efd3c6baf8cdd874d8c806e52e00
Yar-ua <Yar.ua.1985@gmail.com>
2016-08-08 22:14:22 +0300

    update orders button

--- public/app.js
+++ public/app.js
@@ -17,7 +17,22 @@ function add_to_cart(id)
 		x = x * 1 + 1;
 		window.localStorage.setItem(key, x);
 
-		alert('Items in your cart: ' + cart_get_number_of_items());		//вывод кол-ва Itemов в корзине
+		update_orders_input();
+		update_orders_button();
+	}
+
+
+function update_orders_input()
+	{
+		var orders = cart_get_orders();
+		$('#orders_input').val(orders);
+	}
+
+
+function update_orders_button()
+	{
+		var text = 'Cart (' + cart_get_number_of_items() + ')';
+		$('#orders_button').val(text);
 	}
 
 
@@ -36,4 +51,22 @@ function cart_get_number_of_items()
 			}
 		}
 		return cnt;
+	}
+
+
+function cart_get_orders()
+	{	
+		var orders = '';
+
+		for(var i = 0; i < window.localStorage.length; i++)
+		{
+			var key = window.localStorage.key(i);				//получаем ключ хеша
+			var value = window.localStorage.getItem(key);		//получаем значение по ключу хеша 
+
+			if (key.indexOf('product_') == 0)
+			{
+				orders = orders + key + '=' + value + ',';
+			}
+		}
+		return orders;
 	}
